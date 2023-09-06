ac5351cde314667e0167cbe29da8914d4c12e447
TAEHONG <hong2wrd@gmail.com>
2023-09-06 16:52:39 +0900

    코드 정리

--- src/com/hs/co/controller/CO1200Controller.java
+++ src/com/hs/co/controller/CO1200Controller.java
@@ -1,21 +1,18 @@
 package com.hs.co.controller;
 
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpSession;
-
+import com.hs.co.service.CO1200Service;
+import com.hs.home.controller.UserInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.hs.co.service.CO1200Service;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class CO1200Controller {
@@ -34,8 +31,7 @@ public class CO1200Controller {
    	 * @return	String 	result	판매품의서관리 페이지ID
    	 */
 	@RequestMapping(value = "/co1200")
-	public String co1200(Locale locale, Model model) {
-
+	public String co1200() {
 		return "CO/CO1200";
 	}
 
@@ -47,13 +43,12 @@ public class CO1200Controller {
    	 */
 	@RequestMapping(value = "/co1200Sel")
 	public @ResponseBody List<Map<String, Object>> CO1200_SEL(@RequestBody Map<String, Object> param, HttpSession session) {
-
-		List<Map<String, Object>> list = co1200Service.co1200Sel(param, session);
-		return list;
+		UserInfo user = (UserInfo) session.getAttribute("User");
+		return co1200Service.co1200Sel(param, user);
 	}
 
 	@RequestMapping(value = "/co1200SelInfo")
-	public @ResponseBody int[] CO1200_SEL_INFO(@RequestBody Map<String, Object> param, HttpSession session) {
+	public @ResponseBody int[] CO1200_SEL_INFO(@RequestBody Map<String, Object> param) {
 
 		int[] info = new int[2];
 		int expensePrice = co1200Service.co1200SelExpense(param);
--- src/com/hs/co/service/CO1200Service.java
+++ src/com/hs/co/service/CO1200Service.java
@@ -1,15 +1,12 @@
 package com.hs.co.service;
 
-import java.util.List;
-import java.util.Map;
-
-import javax.inject.Inject;
-import javax.servlet.http.HttpSession;
-
+import com.hs.home.controller.UserInfo;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Service;
 
-import com.hs.home.controller.UserInfo;
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Map;
 
 @Service("co1200Service")
 public class CO1200Service {
@@ -24,14 +21,11 @@ public class CO1200Service {
 	 * @param Map 		param 검색조건 (조회기간, 판품번호, 성명, 부서)
 	 * @return List 	list 법인카드 사용현황 목록
 	 */
-	public List<Map<String, Object>> co1200Sel(Map<String, Object> param, HttpSession session) {
+	public List<Map<String, Object>> co1200Sel(Map<String, Object> param, UserInfo user) {
 		
-		UserInfo vo = (UserInfo) session.getAttribute("User");
-		param.put("USER_ID", vo.getUSER_ID());
-		
-		List<Map<String, Object>> rList = sqlSession.selectList("co1200Mapper.co1200Sel", param);
-		
-		return rList;
+		param.put("USER_ID", user.getUSER_ID());
+
+		return sqlSession.selectList("co1200Mapper.co1200Sel", param);
 	}
 
 	public int co1200SelExpense(Map<String, Object> param) {
