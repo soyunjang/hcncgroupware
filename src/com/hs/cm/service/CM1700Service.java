73351c44ed0d240cfc68a0b235f8ba3f28e7b2e2
TAEHONG <hong2wrd@gmail.com>
2023-09-08 12:43:52 +0900

    맞지 않는 주석 삭제

--- src/com/hs/cm/service/CM1700Service.java
+++ src/com/hs/cm/service/CM1700Service.java
@@ -15,15 +15,7 @@ public class CM1700Service {
 	@Inject
 	private SqlSession sqlSession;
 
-	/**
-   	 * 메소드 설명 : 접속조회, 메뉴접근조회, 권한그룹변경조회
-   	 * -------------------------------------------------------------------
-   	 * @param	Map		param	검색조건 (검색시간)
-   	 * @return	List	list	접속 목록, 메뉴접근 목록, 권한그룹변경 목록
-   	 */
 	public List<HashMap<String, Object>> cm1700LogSel(Map<String, Object> param) {
-		
-		List<HashMap<String, Object>> rList = sqlSession.selectList("cm1700Mapper.cm1700LogSel", param);
-		return rList;
+		return sqlSession.selectList("cm1700Mapper.cm1700LogSel", param);
 	}
 }
