a8e1760c25be4110494b735cf1bd1bdbcce2de28
TAEHONG <hong2wrd@gmail.com>
2023-09-20 17:27:47 +0900

    불필요한 코드 삭제

--- src/com/hs/main/controller/MainController.java
+++ src/com/hs/main/controller/MainController.java
@@ -1,56 +1,33 @@
 package com.hs.main.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.hs.common.service.CommonService;
-import com.hs.home.controller.UserInfo;
-import com.hs.main.service.MainService;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class MainController {
 	
-	@Resource(name="mainService")
-	private MainService mainService;
-	
-	@Resource(name="commonService")
-	private CommonService commonService;
-	
-	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
-	
 	@RequestMapping(value = "/main")
-	public ModelAndView main(Locale locale, Model model, HttpSession session, HttpServletRequest request){
-		ModelAndView mv = new ModelAndView();
-		
-		UserInfo vo = (UserInfo) session.getAttribute("User");
-		
+	public ModelAndView main(HttpSession session, ModelAndView mv){
+
 		mv.setViewName("main");
-		mv.addObject("USER", vo);
+		mv.addObject("USER", session.getAttribute("User"));
 		
 		return mv;
 	}
-	
+
+	@ResponseBody
 	@RequestMapping(value = "/sessionExtend")
-	public @ResponseBody Map <String, Object> sessionExtend(HttpSession session){
-		Map <String, Object> rtnMap = new HashMap<String, Object>();
+	public Map <String, Object> sessionExtend(HttpSession session){
+		Map <String, Object> rtnMap = new HashMap<>();
 		
 		session.setMaxInactiveInterval(session.getMaxInactiveInterval() + 600);
 		
