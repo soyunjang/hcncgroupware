e5289ee2b5aff5a862c7b3c669eaf526c8a65a93
TAEHONG <hong2wrd@gmail.com>
2023-09-22 11:54:17 +0900

    메시지 클래스 생성

---
+++ src/com/hs/util/Message.java
@@ -0,0 +1,9 @@
+package com.hs.util;
+
+public abstract class Message {
+
+    public static final String BAD_REQUEST = "잘못된 요청입니다.";
+    public static final String BAD_REQUEST_USER = "잘못된 사용자입니다.";
+    public static final String BAD_REQUEST_PDF = "PDF 파일 찾기에 실패하였습니다.";
+
+}
